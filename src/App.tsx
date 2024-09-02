c3948c20d50ad3696af8045d0850d6fda55e4fcb
JeanS <jean.sinche@alarcongroup.us>
2024-09-02 16:19:43 -0500

    end reactivity

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import './App.css'
 import Form from './components/Form';
 import CalorieTracker from './components/CalorieTracker';
 import ActivityList from './components/ActivityList';
+import { useEffect, useMemo, useReducer } from 'react';
+import { activityReducer, initialState } from './reducers/activity-reducer';
 
 function App() {
+  const [state, dispatch] = useReducer(activityReducer, initialState)
+  useEffect(() => {
+    localStorage.setItem('activities', JSON.stringify(state.activities))
+  }, [state.activities])
+  const canRestartApp = useMemo(() => state.activities.length, [state.activities])
   return (
     <>
       <header className='bg-lime-600 py-3'>
@@ -11,7 +18,10 @@ function App() {
           <h1 className='text-center text-lg font-bold uppercase'>
             Contador de Calorias
           </h1>
-          <button className="bg-gray-800 hover:bg-gray-900 text-white p-2 font-bold uppercase cursor-pointer rounded-lg text-sm disabled:opacity-10">
+          <button className="bg-gray-800 hover:bg-gray-900 text-white p-2 font-bold uppercase cursor-pointer rounded-lg text-sm disabled:opacity-10"
+            disabled={!canRestartApp}
+            onClick={() => dispatch({ type: 'restart-app' })}
+          >
             Reiniciar App
           </button>
         </div>
@@ -19,18 +29,26 @@ function App() {
 
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
-          <Form />
+          <Form
+            dispatch={dispatch}
+            state={state}
+          />
         </div>
       </section>
 
       <section className='bg-gray-800 py-10'>
         <div className='max-w-4xl mx-auto'>
-          <CalorieTracker />
+          <CalorieTracker
+            activities={state.activities}
+          />
         </div>
       </section>
 
       <section className="p-10 mx-auto max-w-4xl">
-        <ActivityList />
+        <ActivityList
+          activities={state.activities}
+          dispatch={dispatch}
+        />
       </section>
     </>
   )
--- src/components/CalorieDisplay.tsx
+++ src/components/CalorieDisplay.tsx
@@ -1,8 +1,12 @@
-export default function CalorieDisplay() {
+type CalorieDisplayProps = {
+    calories: number;
+    text: string;
+}
+export default function CalorieDisplay({ calories, text }: CalorieDisplayProps) {
     return (
         <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-            <span className="font-black text-6xl text-orange-600">Hola</span>
-            Holax2
+            <span className="font-black text-6xl text-orange-600">{calories}</span>
+            {text}
         </p>
     )
 }
